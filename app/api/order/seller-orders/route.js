219ad107a54c6a0311fc1f9770a5e007601dfb35
AllOff <AllOff@example.com>
2025-05-26 12:26:47 +0800

    Initial commit

---
+++ app/api/order/seller-orders/route.js
@@ -0,0 +1,29 @@
+import { getAuth } from "@clerk/nextjs/server";
+import authSeller from "@/lib/authSeller";
+import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
+import Address from "@/models/Address";
+
+export async function GET(request) { 
+    try {
+     
+        const {userId} = getAuth(request);
+        
+        const isSeller = await authSeller(userId);
+
+        if (!isSeller) { 
+            return NextResponse.json({success: false, message: 'not authorized'});
+        }
+
+        await connectDB();
+
+        Address.length
+
+        const orders = await Order.find({}).populate('address items.product')
+
+        return NextResponse.json({success: true, orders});
+
+    } catch (error) {
+        return NextResponse.json({success: false, message: error.message});
+    }
+}
